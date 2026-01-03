66b2ca7df0b43967a00ce4172a1a7495a692633f
Chaitali Shirsath <chaitali3611@gmail.com>
2026-01-03 11:51:02 +0530

    Practice of JAVA

---
+++ Data Types/increament.java
@@ -0,0 +1,11 @@
+class Data
+{
+    public static void main(String a[])
+    {
+        //literals
+        
+        char c = 'a';
+        c++;
+        System.out.println(c);
+    }
+}
---
+++ Data Types/type.java
@@ -0,0 +1,35 @@
+class Data
+{
+    public static void main(String a[])
+    {
+        int num = 9;
+        byte by = 127;
+        short sh = 557;
+        long l = 5564l;
+        float f = 5.6f;
+        double d = 5.6;
+        char c = 't';
+        boolean b = true;
+        
+    }
+}
+
+//for boolean to int
+class Data
+{
+    public static void main(String a[])
+    {
+        int num  = 0b101;
+        System.out.println(num);
+    }
+}
+
+//for hexadecimal to int
+class Data
+{
+    public static void main(String a[])
+    {
+        int num  = 0x101;
+        System.out.println(num);
+    }
+}
