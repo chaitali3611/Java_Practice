e804d5ccff6e12216270cb1f8185b9a70b9de587
Chaitali Shirsath <chaitali3611@gmail.com>
2026-01-05 16:13:08 +0530

    Practicing JAVA

---
+++ print on new line/same line.java
@@ -0,0 +1,29 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        System.out.print(3 + 5);
+        System.out.print(7 + 8);
+    }
+}
+
+for next line
+class Hello
+{
+    public static void main(String a[])
+    {
+        System.out.println(3 + 5);
+        System.out.println(7 + 8);
+    }
+}
+
+class Hello
+{
+    public static void main(String a[])
+    {
+       int num1 = 3;
+       int num2 = 5;
+       int result = num1 + num2;
+       System.out.print(result);
+    }
+}
