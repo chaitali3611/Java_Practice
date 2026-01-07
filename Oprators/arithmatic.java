8fbc173036b7869d462d59ddc1ed2df0f2cc2edc
Chaitali Shirsath <chaitali3611@gmail.com>
2026-01-07 21:37:44 +0530

    JAVA Practice

---
+++ Oprators/arithmatic.java
@@ -0,0 +1,35 @@
+class Data
+{
+    public static void main(String arg[])
+    {
+        //addition
+        int a = 2;
+        int b = 3;
+        int result = a + b;
+        System.out.println(result);
+        
+        //substraction
+        int c = 2;
+        int d = 4;
+        int result = c + d;
+        System.out.println(result);
+        
+        //multipication
+        int a = 4;
+        int b = 8;
+        int result = a * b;
+        System.out.println(result);
+        
+        //division
+        int c = 9;
+        int d = 8;
+        int result = c / d;
+        System.out.println(result);
+        
+        //remainder
+        int a = 5;
+        int b = 4;
+        int result = a / b;
+        System.out.println(result);
+    }
+}
---
+++ Oprators/relative.java
@@ -0,0 +1,30 @@
+class Operator
+{
+    public static void main(String arg[])
+    {
+        double a = 4.5;
+        double b = 4;
+        boolean result = a >= b;
+        
+        System.out.println(result);
+    }
+}
+
+
+//AND & OR
+class Relative
+{
+    public static void main(String arg[])
+    {
+        int x = 2;
+        int y = 4;
+        
+        int a = 3;
+        int b = 9;
+        
+        boolean result = x < y && a < b;
+        
+        System.out.println(result);
+        
+    }
+}
