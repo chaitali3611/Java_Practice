5afee74350be0189651b779abdf8e7f640ca6826
Chaitali Shirsath <chaitali3611@gmail.com>
2026-01-08 12:25:41 +0530

    Turnary Operators in JAVA

---
+++ Turnary Operators/turnary.java
@@ -0,0 +1,34 @@
+class Turnury
+{
+    public static void main(String arg[])
+    {
+        int n = 4;
+        int result = 0;
+        result = n%2==0 ? 10 : 20;
+        System.out.println(result);
+    }
+}
+
+
+//Turnary operators
+class Turnury
+{
+    public static void main(String arg[])
+    {
+        int n = 6;
+        if (n == 1)
+            System.out.println("Monday");
+        else if(n == 2)
+            System.out.println("Tuesday");
+        else if (n == 3)
+            System.out.println("Wednesday");
+        else if (n == 4)
+            System.out.println("Thursday");
+        else if (n == 5)
+            System.out.println("Friday");
+        else if (n == 6)
+            System.out.println("Saturday");
+        else
+            System.out.println("Sunday");
+    }
+}
